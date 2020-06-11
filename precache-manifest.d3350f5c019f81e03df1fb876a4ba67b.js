fde7d16f30c8150bd254b45ba74382826ebad65e
Edwin Narvaez <edwin.narvaez@ucuenca.edu.ec>
2020-06-11 16:51:23 -0500

    Updates

---
+++ precache-manifest.d3350f5c019f81e03df1fb876a4ba67b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc7818e27904e2045cbb90fda73ec1e",
+    "url": "/Cine-con-React/index.html"
+  },
+  {
+    "revision": "f31edf511cf61924e35c",
+    "url": "/Cine-con-React/static/css/2.768c92ad.chunk.css"
+  },
+  {
+    "revision": "0983d5ce095721447d94",
+    "url": "/Cine-con-React/static/css/main.75983624.chunk.css"
+  },
+  {
+    "revision": "f31edf511cf61924e35c",
+    "url": "/Cine-con-React/static/js/2.603b1224.chunk.js"
+  },
+  {
+    "revision": "0983d5ce095721447d94",
+    "url": "/Cine-con-React/static/js/main.ec81399a.chunk.js"
+  },
+  {
+    "revision": "0976d8a9499d3a654b41",
+    "url": "/Cine-con-React/static/js/runtime-main.2970b9c6.js"
+  },
+  {
+    "revision": "2a859fc261205b6926ca83100befd7d1",
+    "url": "/Cine-con-React/static/media/Captura.2a859fc2.JPG"
+  },
+  {
+    "revision": "c310622143bed31afc21078f27243d51",
+    "url": "/Cine-con-React/static/media/Captura2.c3106221.JPG"
+  },
+  {
+    "revision": "3caedd8e7b3cd582599efa25171da352",
+    "url": "/Cine-con-React/static/media/Captura3.3caedd8e.JPG"
+  },
+  {
+    "revision": "3a62c7ae504ce6ee060a9140ce4f123d",
+    "url": "/Cine-con-React/static/media/cine.3a62c7ae.jpg"
+  },
+  {
+    "revision": "88cf9b6d4afc6deb925a0633810b23c6",
+    "url": "/Cine-con-React/static/media/cine.88cf9b6d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Cine-con-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Cine-con-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Cine-con-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Cine-con-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Cine-con-React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
